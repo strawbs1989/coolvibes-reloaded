11907d9d7e35681c4b48ea8348aa1fe8fad1c3bb
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2023-03-31 23:24:03 +0100

    update

---
+++ LnJ/wpscripts/nav_409tree.js
@@ -0,0 +1,22 @@
+var nav_409tree = WpNavBar.readTree({
+"childArray" : [
+{   'sTitle':'Home',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'index.html',
+'sTarget':'_self',"childArray" : [
+{   'sTitle':'Home(mobile)',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'m.index.html',
+'sTarget':'_self'
+}]
+},
+{   'bIsCurrentPage':true,
+'sTitle':'Home(mobile)',
+'bIsExternal':true,
+'bIsWebPath':true,
+'sUrl':'m.index.html',
+'sTarget':'_self'
+}]
+});
