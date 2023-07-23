d9c814b9670ccacd777c9a383c0a6dc242c645b4
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2023-07-23 12:18:14 +0100

    Update showdj.js

--- scripts/showdj.js
+++ scripts/showdj.js
@@ -22,8 +22,8 @@ if(( dy === 3 && hr === 8 && mn <=00 )||
    ( dy === 3 && hr === 8 && mn <=15 )) {
      dj.src='images/off-air.png';
  }
-if ( dy === 4 && hr === 20 && mn <=12 ) {
-     dj.src='images/strawbs.png';
+if ( dy === 4 && hr === 21 && mn <=12 ) {
+     'Thursday at 9 PM<br>Request<br>With Strawbs'dj.src='images/strawbs.png';
  }
 else {
 if(( dy === 5 && hr === 13 && mn <=59 )||
@@ -34,6 +34,10 @@ else {
 if(( dy === 6 && hr === 8 && mn <=14 )||
    ( dy === 6 && hr === 9 && mn <=15 )) {
      'Saturday at 8 AM<br>Christian Music<br>with Dj Bunny'; 
+ }
+if 
+   ( dy === 7 && hr === 21 && mn <=00 )) {
+     'Sundayday at 9 PM<br>Request<br>with Strawbs'dj.src='images/strawbs.png';
  }
  
 else {
