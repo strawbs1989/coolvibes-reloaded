bce78decbcf77e1dfa8db0bf2dc0fa69ad20d8fb
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2023-07-24 17:24:26 +0100

    Update showdj.js
    updated the script

--- scripts/showdj.js
+++ scripts/showdj.js
@@ -1,53 +1,52 @@
-(function( d ) {
-   'use strict';
-function onair(){
-  
-   var dj = d.querySelector('#dj'),
-       nd = new Date(),
-       dy = nd.getDay(),
-       hr = nd.getHours(),                                  
-       mn = nd.getMinutes();
-else {
-if(( dy === 1 && hr === 21 && mn <=00 )||
-   ( dy === 1 && hr === 8 && mn <=15 )) {
-     dj.src='images/billy.png';
- }
- else {
-if(( dy === 2 && hr === 8 && mn <=00 )||
-   ( dy === 2 && hr === 8 && mn <=15 )) {
-     dj.src='images/kayley.png';
- }
- else {
-if(( dy === 3 && hr === 8 && mn <=00 )||
-   ( dy === 3 && hr === 8 && mn <=15 )) {
-     dj.src='images/off-air.png';
- }
-if ( dy === 4 && hr === 21 && mn <=12 ) {
-     'Thursday at 9 PM<br>Request<br>With Strawbs'dj.src='images/strawbs.png';
- }
-else {
-if(( dy === 5 && hr === 13 && mn <=59 )||
-   ( dy === 5 && hr === 14 && mn <=59 )) {
-     'Friday at 10 AM<br>Christian Music<br>with Dj Bunny'; 
- }
-else {
-if(( dy === 6 && hr === 8 && mn <=14 )||
-   ( dy === 6 && hr === 9 && mn <=15 )) {
-     'Saturday at 8 AM<br>Christian Music<br>with Dj Bunny'; 
- }
-if 
-   ( dy === 7 && hr === 21 && mn <=00 )) {
-     'Sundayday at 9 PM<br>Request<br>with Strawbs'dj.src='images/strawbs.png';
- }
- 
-else {
-     dj.src='images/off-air.png';
-  
-    }
-   }
-  }
- }
+<script>
+  (function(d) {
+    function onair() {
+      const dj = d.querySelector('#dj'),
+            tx = d.querySelector('#txt'),
+            nd = new Date(),
+            dy = nd.getDay(),
+            hr = nd.getHours(),                               
+            mn = nd.getMinutes();
 
-setInterval( function(){ onair();}, 1000 );
-}
-}( document )) ;
+      switch (dy) {
+        case 1: // Monday
+          if ((hr === 21 && mn <=59) || (hr === 8 && mn <=15))
+            dj.src = 'images/billy.png';
+          break;
+        case 2: // Tuesday
+          if ((hr === 8 && mn >=0) || (hr === 8 && mn <=15))
+            dj.src = 'images/kayley.png';
+          break;
+        case 3: // Wednesday
+          if ((hr === 8 && mn >=0) || (hr === 8 && mn <=15))
+            dj.src = 'images/off-air.png';
+          break;
+        case 4: // Thursday
+          if (hr === 21 && mn <=00) {
+            dj.src = 'images/strawbs.png';
+            tx.innerHTML = 'Thursday at 9 PM<br>Request<br>With Strawbs';
+          }
+          break;
+        case 5: // Friday
+          if ((hr === 13 && mn <=59) || (hr === 14 && mn <=59))
+            tx.innerHTML = 'Friday at 10 AM<br>Christian Music<br>with Dj Bunny';
+          break;
+        case 6: // Saturday
+          if ((hr === 8 && mn <=14) || (hr === 9 && mn <=15))
+            tx.innerHTML = 'Saturday at 8 AM<br>Christian Music<br>with Dj Bunny';
+          break;
+        case 0: // Sunday
+          if (hr === 21 && mn <=00) {
+            dj.src = 'images/strawbs.png';
+            tx.innerHTML = 'Sunday at 9 PM<br>Request<br>with Strawbs';
+          }
+          break;
+        default:
+          dj.src='images/off-air.png';
+          break;
+      }
+    }
+  
+    setInterval(function() { onair(); }, 1000);
+  })(document);
+</script>
