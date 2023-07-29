4633a466979fbb4677728ebd6a075f70512cdb7d
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2023-07-29 22:43:50 +0100

    update

--- scripts/showdj.js
+++ scripts/showdj.js
@@ -1,55 +1,56 @@
-(function(d) {
-  function onair() {
-    const dj = d.querySelector('#dj'),
-          tx = document.createElement('div'),
-          nd = new Date(),
-          dy = nd.getDay(),
-          hr = nd.getHours(),                               
-          mn = nd.getMinutes();
-      
-    tx.style.color = 'white';
-    dj.insertAdjacentElement('afterend', tx);
+(function( d ) {
+   'use strict';
+function onair(){
+ 
+   var dj = d.querySelector('#dj'),
+       nd = new Date(),
+       dy = nd.getDay(),
+       hr = nd.getHours(),
+       mn = nd.getMinutes();
 
-    switch (dy) {
-      case 1: // Monday
-        if ((hr === 8 && mn <=0) {
-          dj.src = 'https://drive.google.com/file/d/1K6Ki9fxFxVo3UBlA0fjPzLv6D_EpKwIX/view?usp=drive_link';
-        break;
-      case 2: // Tuesday
-        if ((hr === 8 && mn >=0) || (hr === 8 && mn <=15))
-          dj.src = 'https://drive.google.com/file/d/17QW0zuz_PDgUZgXvCz7ybvK9acYraK4K/view?usp=drive_link';
-        break;
-      case 3: // Wednesday
-        if ((hr === 8 && mn >=0) || (hr === 8 && mn <=15))
-          dj.src = 'https://drive.google.com/file/d/1D77ONt7JnNX1rTpNZd6lLYzfTCrXEAcJ/view?usp=drive_link';
-        break;
-      case 4: // Thursday
-        if (hr === 21 && mn >=0) {
-          dj.src = 'https://drive.google.com/file/d/1SaYUrmEwalwxXwygXP0ZthYLKW7aji_H/view?usp=drive_link';
-          tx.innerHTML = 'Thursday at 9 PM<br>Request<br>With Strawbs';
-        }
-        break;
-      case 5: // Friday
-        if ((hr === 13 && mn <=59) || (hr === 14 && mn <=59))
-          tx.innerHTML = 'Friday at 10 AM<br>Christian Music<br>with Dj Bunny';
-        break;
-      case 6: // Saturday
-        if ((hr === 2 && mn <=03)) {
-          dj.src = 'https://drive.google.com/file/d/1D77ONt7JnNX1rTpNZd6lLYzfTCrXEAcJ/view?usp=drive_link';
-          tx.innerHTML = 'Saturday at 8 AM<br>Christian Music<br>with Dj Bunny';
-        }
-        break;
-      case 0: // Sunday
-        if (hr === 21 && mn >=0) {
-          dj.src = 'https://drive.google.com/file/d/1SaYUrmEwalwxXwygXP0ZthYLKW7aji_H/view?usp=drive_link';
-          tx.innerHTML = 'Sunday at 9 PM<br>Request<br>with Strawbs';
-        }
-        break;
-      default:
-        dj.src='https://drive.google.com/file/d/1D77ONt7JnNX1rTpNZd6lLYzfTCrXEAcJ/view?usp=drive_link';
-        break;
+if(( dy === 1 && hr === 8 && mn >=0 )||
+   ( dy === 1 && hr === 18 && mn <=20 )||
+    {
+     dj.src='djimages/kayley.png';
+ }
+ else {
+ if(( dy === 2 && hr === 8 && mn >=0 )||
+    ( dy === 2 && hr === 18 && mn <=20 )||
+    {
+     dj.src='djimages/kayley.png';
+ }	 
+else {
+if(( dy === 3 && hr === 13 && mn <=59 )||
+   ( dy === 3 && hr === 20 && mn <=59 )) {
+     dj.src='djimages/off-air.jpg';
+ }
+else {
+if(( dy === 4 && hr === 8 && mn <=0 )||
+   ( dy === 4 && hr === 9 && mn <=59 )) {
+     dj.src='djimages/strawbs.png';
+else {
+if(( dy === 5 && hr === 21 && mn >=0 )||
+   ( dy === 5 && hr === 18 && mn <=20 )||
+    {
+     dj.src='djimages/off-air.jpg';
+else }
+if(( dy === 6 && hr === 21 && mn >=0 )||
+   ( dy === 6 && hr === 18 && mn <=20 )||
+    {
+     dj.src='djimages/off-air.jpg';
+
+else  }
+if(( dy === 7 && hr === 21 && mn >=0 )||
+   ( dy === 7 && hr === 22 && mn <=0 )||
+    {
+     dj.src='djimages/strawbs.png';
+ }
+else {
+     dj.src='djimages/off-air.jpg';
     }
+   }
   }
+ }
 
-  setInterval(function() { onair(); }, 1000);
-})(document);
+setInterval( function(){ onair();}, 1000 );
+}( document ));
