608764cb0d12c9a4e352e3b617628b2f0be471ae
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2023-07-29 01:33:24 +0100

    Update showdj.js

--- scripts/showdj.js
+++ scripts/showdj.js
@@ -1,53 +1,56 @@
-(function(d) {
-    function onair() {
-      const dj = d.querySelector('#dj'),
-            tx = document.createElement('div'),
-            nd = new Date(),
-            dy = nd.getDay(),
-            hr = nd.getHours(),                               
-            mn = nd.getMinutes();
-        
-      tx.style.color = 'white';
-      dj.insertAdjacentElement('afterend', tx);
+(function( d ) {
+   'use strict';
+function onair(){
+  
+   var dj = d.querySelector('#dj'),
+       nd = new Date(),
+       dy = nd.getDay(),
+       hr = nd.getHours(),
+       mn = nd.getMinutes();
 
-      switch (dy) {
-        case 1: // Monday
-          if ((hr === 21 && mn <=59) || (hr === 8 && mn <=15))
-            dj.src = 'https://github.com/strawbs1989/coolvibes-reloaded/tree/main/images/billy.png';
-          break;
-        case 2: // Tuesday
-          if ((hr === 8 && mn >=0) || (hr === 8 && mn <=15))
-            dj.src = 'https://github.com/strawbs1989/coolvibes-reloaded/tree/main/images/kayley.png';
-          break;
-        case 3: // Wednesday
-          if ((hr === 8 && mn >=0) || (hr === 8 && mn <=15))
-            dj.src = 'https://github.com/strawbs1989/coolvibes-reloaded/blob/main/images/off-air.jpg';
-          break;
-        case 4: // Thursday
-          if (hr === 21 && mn >=0) {
-            dj.src = 'https://github.com/strawbs1989/coolvibes-reloaded/tree/main/images/strawbs.png';
-            tx.innerHTML = 'Thursday at 9 PM<br>Request<br>With Strawbs';
-          }
-          break;
-        case 5: // Friday
-        if (hr >= 10 && hr <= 23)
-        tx.innerHTML = 'Friday at 10 AM<br>Christian Music<br>with Dj Bunny';
-         break;
-        case 6: // Saturday
-          if ((hr === 8 && mn <=14) || (hr === 9 && mn <=15))
-            tx.innerHTML = 'Saturday at 8 AM<br>Christian Music<br>with Dj Bunny';
-          break;
-        case 0: // Sunday
-          if (hr === 21 && mn >=0) {
-            dj.src = 'https://github.com/strawbs1989/coolvibes-reloaded/tree/main/images/strawbs.png';
+if(( dy === 1 && hr === 21 && mn >=00 )||
+   ( dy === 1 && hr === 8 && mn <=00 )||
+   ( dy === 1 && hr === 6 && mn >=20 )||
+   ( dy === 1 && hr === 7 && mn <=19 )) {
+     dj.src='https://github.com/strawbs1989/coolvibes-reloaded/tree/main/images/billy.png';
+ }
+else {
+if(( dy === 2 && hr === 8 && mn <=00 )||
+   ( dy === 2 && hr === 14 && mn <=59 )) {
+     dj.src='https://github.com/strawbs1989/coolvibes-reloaded/tree/main/images/kayley.png''; 
+ }
+else {
+if(( dy === 3 && hr === 8 && mn <=59 )||
+   ( dy === 3 && hr === 9 && mn <=59 )) {
+     dj.src='https://github.com/strawbs1989/coolvibes-reloaded/blob/main/images/off-air.jpg'';
+ }
+ else {
+if(( dy === 4 && hr === 21 && mn <=00 )||
+   ( dy === 4 && hr === 21 && mn <=00 )) {
+     dj.src='https://github.com/strawbs1989/coolvibes-reloaded/tree/main/images/strawbs.png'';
+ }
+ else {
+if(( dy === 5 && hr === 8 && mn <=59 )||
+   ( dy === 5 && hr === 9 && mn <=59 )) {
+     tx.innerHTML = 'Friday at 10 AM<br>Christian Music<br>with Dj Bunny';
+ }
+ else {
+if(( dy === 6 && hr === 8 && mn <=59 )||
+   ( dy === 6 && hr === 9 && mn <=59 )) {
+     tx.innerHTML = 'Saturday at 8 AM<br>Christian Music<br>with Dj Bunny';
+ }
+ else {
+if(( dy === 7 && hr === 21 && mn <=00 )||
+   ( dy === 7 && hr === 9 && mn <=59 )) {
+     dj.src = 'https://github.com/strawbs1989/coolvibes-reloaded/tree/main/images/strawbs.png';
             tx.innerHTML = 'Sunday at 9 PM<br>Request<br>with Strawbs';
-          }
-          break;
-        default:
-          dj.src='https://github.com/strawbs1989/coolvibes-reloaded/blob/main/images/off-air.jpg';
-          break;
-      }
+ }
+else {
+     dj.src='https://github.com/strawbs1989/coolvibes-reloaded/blob/main/images/off-air.jpg';
     }
- 
-    setInterval(function() { onair(); }, 1000);
-  })(document);
+   }
+  }
+ }
+
+setInterval( function(){ onair();}, 1000 );
+}( document ));
