2d6afa61da78ac92531ac426a3aa6714ff198732
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2023-07-30 22:15:30 +0100

    update images and js script

--- scripts/showdj.js
+++ scripts/showdj.js
@@ -13,14 +13,14 @@
     switch (dy) {
       case 1: // Monday
         if ((hr === 8) { // 8:00 - 8:59 - 60 minutes - 1 hour
-          dj.src = 'djimages/kayley.png';
+          dj.src = 'djimages/kayley.jpg';
         break;
 		if ((hr === 21) { // 21:00 - 21:59 - 59 minutes - 0 hour
-          dj.src = 'djimages/billy.png';
+          dj.src = 'djimages/billy.jpg';
         break;
       case 2: // Tuesday
         if (hr === 8 && (mn >= 0 && mn <= 15)) // 8:00 - 8:15 - 15 minutes
-          dj.src = 'djimages/kayley.png';
+          dj.src = 'djimages/kayley.jpg';
         break;
       case 3: // Wednesday
         if (hr === 8 && (mn >= 0 && mn <= 15)) // 8:00 - 8:15 - 15 minutes
@@ -28,29 +28,29 @@
         break;
       case 4: // Thursday
         if (hr === 21) { // 21:00 - 21:59 - 60 minutes - 1 hour
-          dj.src = 'djimages/strawbs.png';
+          dj.src = 'djimages/strawbs.jpg';
           tx.innerHTML = 'Thursday at 9 PM<br>Request<br>With Strawbs';
         }
         break;
       case 5: // Friday
                 if (hr === 13 || hr === 14) // 13:00 - 14:59 - 2 hours
-          dj.src = 'djimages/djbunny.png';
+          dj.src = 'djimages/djbunny.jpg';
           tx.innerHTML = 'Saturday at 8 AM<br>Christian Music<br>with Dj Bunny';
         break;
       case 6: // Saturday
         if (hr === 2 && mn >=03) { // 2:03 - 2:59 - 57 minutes
-          dj.src = 'djimages/djbunny.png';
+          dj.src = 'djimages/djbunny.jpg';
           tx.innerHTML = 'Saturday at 8 AM<br>Christian Music<br>with Dj Bunny';
         }
         break;
       case 0: // Sunday
         if (hr === 21) { // 21:00 - 21:59 - 60 minutes - 1 hour
-          dj.src = 'djimages/strawbs.png';
+          dj.src = 'djimages/strawbs.jpg';
           tx.innerHTML = 'Sunday at 9 PM<br>Request<br>with Strawbs';
         }
         break;
       default:
-        dj.src = 'djimages/strawbs.png';
+        dj.src = 'djimages/strawbs.jpg';
         tx.innerHTML = 'Thursday at 9 PM<br>Request<br>With Strawbs';
         break;
     }
