7f4742b8567401918d1b9db4bb76e6a40c9f17d7
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2023-08-06 00:59:15 +0100

    Update showdj.js

--- scripts/showdj.js
+++ scripts/showdj.js
@@ -6,8 +6,7 @@
           dy = nd.getDay(),
           hr = nd.getHours(),                          
           mn = nd.getMinutes();
-		  tx.style.color = 'white';
-          dj.insertAdjacentElement('afterbegin', tx);
+		  let txt = null;
  
     
 
@@ -18,7 +17,7 @@
      
         if (hr === 13) { // 13:00 - 13:59 - 60 minutes - 1 hour
           dj.src = 'djimages/djbunny.jpg';
-          tx.innerHTML = 'Monday at 1 PM<br>Auto<br>With Bunny';
+          
         }
 
         if (hr === 21) // 21:00 - 21:59 - 60 minutes - 1 hour
@@ -30,7 +29,7 @@
          
         if (hr === 13) { // 13:00 - 13:59 - 59 minutes - 1 hour
           dj.src = 'djimages/djbunny.jpg';
-          tx.innerHTML = 'Tuesday at 1 PM<br>Auto<br>With Bunny';
+          
         }
          
         if (hr === 17) // 17:00 - 17:59 - 59 minutes - 1 hour
@@ -51,29 +50,29 @@
            
         if (hr === 16) { // 16:00 - 16:59 - 60 minutes - 1 hour
           dj.src = 'djimages/djbunny.jpg';
-          tx.innerHTML = 'Monday at 1 PM<br>Auto<br>With Bunny';
+          
         }          
         break;
       case 5: // Friday
         if (hr === 13 || hr === 14) { // 13:00 - 14:59 - 1 hours - 59 minutes
           dj.src = 'djimages/djbunny.jpg';
-          tx.innerHTML = 'Saturday at 8 AM<br>Christian Music<br>with Dj Bunny';
+          
         }          
         break;
       case 6: // Saturday
         if (hr === 2 || hr === 3) { // 2:00 - 3:59 - 2 hours
           dj.src = 'djimages/djbunny.jpg';
-          tx.innerHTML = 'Saturday at 8 AM<br>Christian Music<br>with Dj Bunny';
+          
         } 
 
         if (hr === 12 || hr === 13) { // 12:00 - 13:59 - 2 hours
           dj.src = 'djimages/djbunny.jpg';
-          tx.innerHTML = 'Saturday at 12 AM<br>Dance<br>with Dj Bunny';
+          
         } 
 		
 		if (hr === 22 || hr === 23) { // 22:00 - 23:59 - 2 hours
           dj.src = 'djimages/djbunny.jpg';
-          tx.innerHTML = 'Saturday at 10 PM<br>Dance<br>with Dj Bunny';
+          
         } 
 		
         break;
