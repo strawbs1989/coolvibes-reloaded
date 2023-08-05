3036aa698ebfe07481ba4bec70ac451202df36e7
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2023-08-05 22:55:49 +0100

    Update showdj.js

--- scripts/showdj.js
+++ scripts/showdj.js
@@ -7,7 +7,7 @@
           hr = nd.getHours(),                          
           mn = nd.getMinutes();
 		  tx.style.color = 'white';
-          dj.insertAdjacentText('beforeend', tx);
+          dj.insertAdjacentElement('afterbegin', tx);
  
     
 
