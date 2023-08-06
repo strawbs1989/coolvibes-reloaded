7262b6d1e77a9d6d4a138599ee00ebf002017653
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2023-08-06 01:02:15 +0100

    Update showdj.js

--- scripts/showdj.js
+++ scripts/showdj.js
@@ -6,6 +6,7 @@
           dy = nd.getDay(),
           hr = nd.getHours(),                          
           mn = nd.getMinutes();
+		  var = NoShow.'No Show Scheduled<br />for COOLVIBES()';
 		  let txt = null;
  
     
