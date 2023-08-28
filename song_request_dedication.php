b55927e86c001fa21dda9b71333ec16cc4719d19
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2023-08-28 18:24:51 +0100

    updated website

--- song_request_dedication.php
+++ song_request_dedication.php
@@ -1,11 +1,43 @@
-<?php
+    <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve the form data
     $selectedSong = $_POST["song"];
     $name = $_POST["name"];
     $message = $_POST["message"];
 
-    // Perform any necessary processing (e.g., storing in a database, sending emails)
+    // Database connection settings
+    $servername = "sql311.byethost8.com";
+    $username = "b8_34833020";
+    $password = "Laurajay1998";
+    $dbname = "b8_34833020_coolvibes";
+
+    // Create a connection to the database
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check if the connection was successful
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Prepare and execute an SQL query to insert data
+    $sql = "INSERT INTO song_requests (SelectedSong, Name, Message) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $selectedSong, $name, $message);
+    
+    if ($stmt->execute()) {
+        echo "Request submitted successfully!";
+    } else {
+        echo "Error: " . $stmt->error;
+    }
+
+    // Close the database connection
+    $stmt->close();
+    $conn->close();
+} else {
+    echo "Invalid request.";
+}
+?>
+
     
     // Return a response (e.g., success or error message)
     echo "Request submitted successfully!";
