cccb328e28d4f6e347e0b15305722fc0aaa14de9
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2023-09-11 17:50:46 +0100

    form update

--- success.js
+++ success.js
@@ -4,7 +4,7 @@ const handleSubmit = (event) => {
   const myForm = event.target;
   const formData = new FormData(myForm);
   
-  fetch("/", {
+  fetch("https://coolvibes-reloaded.com/reg.html",{credentials: 'omit'}); {
     method: "POST",
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
