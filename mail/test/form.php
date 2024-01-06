5e7296ad86d3f4ffb6f4acaecffad8a77e50b5d5
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-01-06 22:45:07 +0000

    Update form.php

--- mail/test/form.php
+++ mail/test/form.php
@@ -1,6 +1,8 @@
 <?php
-
+$message_sent = false;
 if(isset($_POST)['email']) && $_POST['email'] != ''){
+	if( filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ){
+	
 	// submit the form
     
 	$yourName = $_POST['name'];
@@ -17,6 +19,8 @@ if(isset($_POST)['email']) && $_POST['email'] != ''){
    $body .= "Message: ".$message. "\r\n"
    
    mail($to,$message,$body);
+   
+   $message_sent = true;
 
 }
    
