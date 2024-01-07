607d830d30b056b0a05213b085bbf487ab913aa9
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-01-07 00:22:48 +0000

    Update index.php

--- mail/test/index.php
+++ mail/test/index.php
@@ -25,22 +25,22 @@ else:
         <form action="https://coolvibes-reloaded.com/mail/test/form" method="POST" class="form">
             <div class="form-group">
                 <label for="name" class="form-label">Your Name</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Jane Doe" tabindex="1" required>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Your Name" tabindex="1" required>
             </div>
             <div class="form-group">
-                <label for="email" class="form-label">Your Email</label>
-                <input type="email" class="form-control" id="email" name="email" placeholder="jane@doe.com" tabindex="2" required>
+                <label for="email" class="form-label">Track Name</label>
+                <input type="text" class="form-control" id="name" name="email" placeholder="Track Name" tabindex="2" required>
             </div>
             <div class="form-group">
-                <label for="subject" class="form-label">Subject</label>
-                <input type="text" class="form-control" id="subject" name="subject" placeholder="Hello There!" tabindex="3" required>
+                <label for="subject" class="form-label">Artist Name</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Artist Name!" tabindex="3" required>
             </div>
             <div class="form-group">
-                <label for="message" class="form-label">Message</label>
-                <textarea class="form-control" rows="5" cols="50" id="message" name="message" placeholder="Enter Message..." tabindex="4"></textarea>
+                <label for="message" class="form-label">Dedication Message</label>
+                <textarea class="form-control" rows="5" cols="50" id="message" name="message" placeholder="Dedicate" tabindex="4"></textarea>
             </div>
             <div>
-                <button type="submit" class="btn">Send Message!</button>
+                <button type="submit" class="btn">Request It!</button>
             </div>
         </form>
     </div>
