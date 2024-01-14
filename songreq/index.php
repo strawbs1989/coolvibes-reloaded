f95504da37a030120a74479cfb88d0ab6bb5e7bd
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-01-14 23:21:19 +0000

    Update index.php

--- songreq/index.php
+++ songreq/index.php
@@ -1,17 +1,16 @@
 <?php
 
-if (isset($_POST['dedicate'])) {
 $name =$_POST[name];
 $artistname =$_POST[artistname];	
 $artist_song =$_POST[artisttrack];	
 $dedication =$_POST[dedication];	
 $headers =$_POST[headers];
 
-$mailTo= "requests@requests.cu.ma";
+$mailTo= "coolvibes1989@gmail.com";
 $headers = "From: ".$mailFrom;
 $txt = "You Have received an email from ".$name.".\n\n".$message;
 
 mail($mailTo, $subject, $txt, $headers);
 header("Location: coolvibes-reloaded.com/songreq/index.php?mailsent");	
-}
+
 ?>
