c9ebc3188343af97b766d0c46f37ec9a08a20c0c
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-01-15 15:46:14 +0000

    new update

---
+++ newreq/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+ 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+
+</head>
+ 
+<body>
+  <div class="container">
+    <form id="contact" action="mail.php" method="post">
+      <h1>Requests</h1>
+ 
+      <fieldset>
+        <input placeholder="Your name" name="name" type="text" tabindex="1" autofocus>
+      </fieldset>
+      <fieldset>
+        <input placeholder="Track Name" name="track" type="track" tabindex="2">
+      </fieldset>
+      <fieldset>
+        <input placeholder="Artist Name" type="text" name="artist" tabindex="4">
+      </fieldset>
+      <fieldset>
+        <textarea name="message" placeholder="your Dedication Message..." tabindex="5"></textarea>
+      </fieldset>
+ 
+      <fieldset>
+        <button type="dedicate" name="send" id="contact-dedicate">Dedication Now</button>
+      </fieldset>
+    </form>
+  </div>
+</body>
+ 
+</html>
