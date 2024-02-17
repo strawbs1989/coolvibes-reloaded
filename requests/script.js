0affefc24e9fccfd2581dab8f50f15b7624f12b4
strawbs1989 <103792790+strawbs1989@users.noreply.github.com>
2024-02-17 14:34:49 +0000

    testing

--- requests/script.js
+++ requests/script.js
@@ -1,3 +1,23 @@
-function successCallback(token) {
-  debugger;
-}
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	
+	const captchaResponse = grecaptcha.getResponse();
+	
+	if (caprchaResponse.length > 0) {
+		throw new Error("Caprcha not complete");
+		
+	}
+	
+	const fd = new FormData(e.target);
+	const params = new URLSearchParams(fd);
+	
+	fetch('httpbin.org/post', {
+		method: "POST",
+		body: params,
+	})
+	.then(res => res.json())
+	
+	
+});
